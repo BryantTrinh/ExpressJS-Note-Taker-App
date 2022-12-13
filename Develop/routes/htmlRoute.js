c195b530a049769450c541867e990638e91a3857
Bryant Trinh <trinhbryant@gmail.com>
2022-12-12 19:13:13 -0800

    added more of the shell, finished htmlRoute.js

---
+++ Develop/routes/htmlRoute.js
@@ -0,0 +1,22 @@
+// - Things we need to do GET /notes should return the notes.html file.
+// - GET * should return the index.html file.
+
+// we need to do a path and a router in order to use the route.
+const path = require('path');
+const router = require('express').Router();
+
+// router.get the /notes path and the file. Use path.join dirname + notes.html path
+
+router.get('/notes, (res, res) =>{
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// We use * to make all other routes we have .. apiRoute to respond (res) with index.html
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+// module export this to use
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,5 @@
+// Here we shall require express and have routes for our api and html
+
+const express = require('express');
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
