9cbaacf4915bf67422270563a3ea2ad97789cff2
Bryant Trinh <trinhbryant@gmail.com>
2022-12-14 18:01:54 -0800

    finalized changes

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -35,14 +35,11 @@ router.post('/notes', (req, res) => {
 
 // For bonus, DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
-router.delete('/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res, next) => {
 	store
-  .removeNote(req.params.id);
-	if (err) {
-		res.send(500);
-	} else {
-		res.send(200);
-	};
+		.removeNote(req.params.id)
+		.then(() => res.json({ ok: true }))
+		.catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -5,11 +5,13 @@
 const path = require('path');
 const router = require('express').Router();
 
+
 // router.get the /notes path and the file. Use path.join dirname + notes.html path
 
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+	res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
 
 // We use * to make all other routes we have .. apiRoute to respond (res) with index.html
 
