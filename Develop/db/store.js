98233770436de8e4b7c3b6384726ea7954bb7b2a
Bryant Trinh <trinhbryant@gmail.com>
2022-12-14 18:39:48 -0800

    Adding mroe finalized changes to package.json

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,54 +1,53 @@
 const fs = require('fs');
 const util = require('util');
 
-// package that generates unique ids 
+// package that generates unique ids
 
-const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const uuidv1 = require('uuidv1');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-  read() {
-    return readFileAsync('db/db.json', 'utf8');
-  }
-  write(note) {
-  return writeFileAsync('db/db.json', JSON.stringify(note));
-  }
+	read() {
+		return readFileAsync('db/db.json', 'utf8');
+	}
+	write(note) {
+		return writeFileAsync('db/db.json', JSON.stringify(note));
+	}
 
-  getNotes() {
-    return this.read().then((notes) => {
-      let parsedNotes;
-    try {
-      parsedNotes = [].concat(JSON.parse(notes));
-    } catch (err) {
-      parsedNotes = [];
-    }
-  
-    return parsedNotes;
-    });
-  }  
+	getNotes() {
+		return this.read().then((notes) => {
+			let parsedNotes;
+			try {
+				parsedNotes = [].concat(JSON.parse(notes));
+			} catch (err) {
+				parsedNotes = [];
+			}
 
-  addNote(note) {
-    const { title, text } = note;
-    if(!title || !text) {
-      throw new Error("Note 'title' and 'text' can't be blank");
-    }  
+			return parsedNotes;
+		});
+	}
 
-    const newNote = { title, text, id: uuidv1() };
+	addNote(note) {
+		const { title, text } = note;
+		if (!title || !text) {
+			throw new Error("Note 'title' and 'text' can't be blank");
+		}
 
-    return this.getNotes()
-    .then((notes) => [... notes, newNote])
-    .then((updatedNotes) => this.write(updatedNotes))
-    .then(() => newNote);
-  }
+		const newNote = { title, text, id: uuidv1() };
 
-  removeNote(id) {
-    return this.getNotes()
-    .then((notes) => notes.filter((note) => note.id !== id))
-    .then((filteredNotes) => this.write(filteredNotes));
-  }
+		return this.getNotes()
+			.then((notes) => [...notes, newNote])
+			.then((updatedNotes) => this.write(updatedNotes))
+			.then(() => newNote);
+	}
+
+	removeNote(id) {
+		return this.getNotes()
+			.then((notes) => notes.filter((note) => note.id !== id))
+			.then((filteredNotes) => this.write(filteredNotes));
+	}
 }
 
 module.exports = new Store();
